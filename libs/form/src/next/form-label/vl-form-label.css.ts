ecd107b1b2a5c63dc080aaa0e684a677014bba5b
Karim Dehbi <karim.dehbi@gmail.com>
2024-10-24 18:02:55 +0200

    feat: UIG-3123 - vl-title-next, vl-form-label-next - styling stuurbaar gemaakt
    De CSS van vl-form-label-next gemigreerd.
    Part attributes voorzien voor de HTML van de `vl-title-next` en `vl-form-label-next` om gepaste styling te kunnen aanbieden binnen `vl-search-filter-next`.
    Cypress testen uitgebreid.

---
+++ libs/form/src/next/form-label/vl-form-label.css.ts
@@ -0,0 +1,23 @@
+import { css } from 'lit';
+
+export const formLabelStyles = css`
+    .vl-form__label {
+        font-size: 1.6rem;
+        font-weight: 500;
+        color: #4d4d4b;
+        display: inline-block;
+        line-height: 1;
+        margin-right: 0.5rem;
+        margin-bottom: 0.7rem;
+    }
+
+    .vl-form__label--block {
+        display: block;
+        margin-right: 0;
+        margin-bottom: 0.7rem;
+    }
+
+    .vl-form__label--light {
+        color: #687483;
+    }
+`;
