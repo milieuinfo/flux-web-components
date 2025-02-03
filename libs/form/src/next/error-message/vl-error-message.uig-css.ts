ece7bc1072691ea8678880b74b314e0404dfdd94
Karim Dehbi <karim.dehbi@gmail.com>
2025-02-03 11:06:26 +0100

    feat: UIG-3201 - vl-error-message-next - mogelijkheid meerdere lijnen toegevoegd

--- libs/form/src/next/error-message/vl-error-message.uig-css.ts
+++ libs/form/src/next/error-message/vl-error-message.uig-css.ts
@@ -3,6 +3,7 @@ import { css, CSSResult } from 'lit';
 const styles: CSSResult = css`
     .vl-form__error {
         margin-top: 0.2rem;
+        white-space: pre-line;
     }
 `;
 export default styles;
