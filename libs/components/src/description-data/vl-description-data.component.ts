af48a4b3b489e70b89fd5e12e50acb5b4a204d9a
Karim Dehbi <karim.dehbi@gmail.com>
2025-02-11 16:52:58 +0100

    vl-description-data grid & column replace

--- libs/components/src/description-data/vl-description-data.component.ts
+++ libs/components/src/description-data/vl-description-data.component.ts
@@ -1,41 +1,28 @@
+import { BaseLitElement } from '@domg-wc/common-utilities';
+import { vlGridStyles, vlResetStyles } from '@domg-wc/common-utilities/css';
 import { descriptionDataStyle } from '@domg/govflanders-style/component';
-import { gridStyle, resetStyle } from '@domg/govflanders-style/common';
 import { html } from 'lit';
 import { customElement } from 'lit/decorators.js';
-import { BaseLitElement, registerWebComponents } from '@domg-wc/common-utilities';
-import { VlColumnElement, VlGridElement } from '@domg-wc/elements';
 import { classMap } from 'lit/directives/class-map.js';
 
 @customElement('vl-description-data')
 export class VlDescriptionData extends BaseLitElement {
     private size = 0;
-    private maxSize = 0;
     private mediumSize = 0;
-    private mediumMaxSize = 0;
     private smallSize = 0;
-    private smallMaxSize = 0;
     private extraSmallSize = 0;
-    private extraSmallMaxSize = 0;
     private bordered = false;
 
-    static {
-        registerWebComponents([VlColumnElement, VlGridElement]);
-    }
-
     static get styles() {
-        return [resetStyle, gridStyle, descriptionDataStyle];
+        return [vlResetStyles, descriptionDataStyle, vlGridStyles];
     }
 
     static get properties() {
         return {
             size: { type: Number, attribute: 'data-vl-items-size' },
-            maxSize: { type: Number, attribute: 'data-vl-items-max-size' },
             mediumSize: { type: Number, attribute: 'data-vl-items-medium-size' },
-            mediumMaxSize: { type: Number, attribute: 'data-vl-items-medium-max-size' },
             smallSize: { type: Number, attribute: 'data-vl-items-small-size' },
-            smallMaxSize: { type: Number, attribute: 'data-vl-items-small-max-size' },
             extraSmallSize: { type: Number, attribute: 'data-vl-items-extra-small-size' },
-            extraSmallMaxSize: { type: Number, attribute: 'data-vl-items-extra-small-max-size' },
             bordered: { type: Boolean, attribute: 'data-vl-bordered' },
         };
     }
@@ -51,30 +38,29 @@ export class VlDescriptionData extends BaseLitElement {
     render() {
         this.size = this.size || 12 / this.children.length;
         const classes = {
-            'vl-description-data': true,
             'vl-description-data--bordered': this.bordered,
         };
-        return html` <div class=${classMap(classes)}>
-            <div is="vl-grid">
-                ${[...Array.from(this.children)].map((child, index) => {
-                    const name = `item-${index}`;
-                    child.setAttribute('slot', name);
-                    return html` <div
-                        is="vl-column"
-                        data-vl-size=${this.size}
-                        data-vl-max-size=${this.maxSize}
-                        data-vl-medium-size=${this.mediumSize}
-                        data-vl-medium-max-size=${this.mediumMaxSize}
-                        data-vl-small-size=${this.smallSize}
-                        data-vl-small-max-size=${this.smallMaxSize}
-                        data-vl-extra-small-size=${this.extraSmallSize}
-                        data-vl-extra-small-max-size=${this.extraSmallMaxSize}
-                    >
-                        <slot name=${name}></slot>
-                    </div>`;
-                })}
+        const columnClasses = {
+            [`vl-column-next--${this.size}`]: this.size,
+            [`vl-column-next--m-${this.mediumSize}`]: this.mediumSize,
+            [`vl-column-next--s-${this.smallSize}`]: this.smallSize,
+            [`vl-column-next--xs-${this.extraSmallSize}`]: this.extraSmallSize,
+        };
+        return html`
+            <div class="vl-description-data ${classMap(classes)}">
+                <div class="vl-grid-next">
+                    ${[...Array.from(this.children)].map((child, index) => {
+                        const name = `item-${index}`;
+                        child.setAttribute('slot', name);
+                        return html`
+                            <div class="vl-column-next ${classMap(columnClasses)}">
+                                <slot name=${name}></slot>
+                            </div>
+                        `;
+                    })}
+                </div>
             </div>
-        </div>`;
+        `;
     }
 }
 
