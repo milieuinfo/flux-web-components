bf6d3e08879dd8b22523339a0913c80c9a73e9a0
Karim Dehbi <karim.dehbi@gmail.com>
2025-03-20 16:23:39 +0100

    vl-content-header.stories.ts - OK

--- libs/components/src/content-header/stories/vl-content-header.stories.ts
+++ libs/components/src/content-header/stories/vl-content-header.stories.ts
@@ -23,7 +23,6 @@ const Template = story(
     ({ contextLink, titleLink }) => html`
         <vl-content-header>
             <img
-                is="vl-image"
                 slot="image"
                 sizes="100vw"
                 src="https://images.unsplash.com/photo-1561070791-2526d30994b5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80"
