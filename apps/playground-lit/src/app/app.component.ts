6f5df91025cf0763f31782bc9815c07c9f4c2804
Karim Dehbi <karim.dehbi@gmail.com>
2025-03-20 16:05:22 +0100

    playground-lit - not important

--- apps/playground-lit/src/app/app.component.ts
+++ apps/playground-lit/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { registerWebComponents } from '@domg-wc/common-utilities';
-import { VlTabsComponent } from '@domg-wc/components';
+import { vlGridStyles } from '@domg-wc/common-utilities/css';
+import { VlHttpErrorMessage, VlTabsComponent } from '@domg-wc/components';
 import { vlElementsStyle } from '@domg-wc/elements';
 import { SelectRichOption, VlSelectRichComponent } from '@domg-wc/form/next/select-rich';
 import { CSSResult, html, LitElement } from 'lit';
@@ -8,11 +9,11 @@ import { customElement } from 'lit/decorators.js';
 @customElement('app-component')
 export class AppComponent extends LitElement {
     static {
-        registerWebComponents([VlTabsComponent, VlSelectRichComponent]);
+        registerWebComponents([VlTabsComponent, VlSelectRichComponent, VlHttpErrorMessage]);
     }
 
     static get styles(): (CSSResult | CSSResult[])[] {
-        return [vlElementsStyle];
+        return [vlElementsStyle, vlGridStyles];
     }
 
     constructor() {
@@ -21,32 +22,12 @@ export class AppComponent extends LitElement {
 
     render() {
         return html`
-            <main>
-                <vl-tabs data-vl-active-tab="trein" data-vl-disable-links="">
-                    <vl-tabs-pane data-vl-id="trein" data-vl-title="Trein">
-                        <vl-select-rich-next
-                            style="width: 300px; display: block"
-                            id="geboorteplaats"
-                            name="geboorteplaats"
-                            placeholder="Kies je geboorteplaats"
-                            .options=${this.geboorteplaatsen}
-                            search=""
-                        >
-                        </vl-select-rich-next>
-                    </vl-tabs-pane>
-                    <vl-tabs-pane data-vl-id="metro" data-vl-title="Metro, tram en bus">
-                        Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Etiam
-                        porta sem malesuada magna mollis euismod. Morbi leo risus, porta ac consectetur ac, vestibulum
-                        at eros. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    </vl-tabs-pane>
-                    <vl-tabs-pane data-vl-id="fiets" data-vl-title="Fiets">
-                        Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.
-                        Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cras justo odio,
-                        dapibus ac facilisis in, egestas eget quam. Praesent commodo cursus magna, vel scelerisque nisl
-                        consectetur et.
-                    </vl-tabs-pane>
-                </vl-tabs>
-            </main>
+            <vl-http-error-message
+                data-vl-title="Niets gevonden hiervoor."
+                data-vl-image="https://static.freeimages.com/assets/icons/logo.svg"
+                data-vl-image-alt="Niets gevonden"
+            >
+            </vl-http-error-message>
         `;
     }
 
