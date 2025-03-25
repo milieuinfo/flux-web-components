df8541fe70dc48e492dd1e6f5c7de56e79d1025f
Karim Dehbi <karim.dehbi@gmail.com>
2025-03-25 16:30:56 +0100

    fix vl-popover-menu-accordion.component.cy.ts

--- libs/integration/src/popover/menu-accordion/vl-popover-menu-accordion.component.cy.ts
+++ libs/integration/src/popover/menu-accordion/vl-popover-menu-accordion.component.cy.ts
@@ -15,20 +15,20 @@ describe('integration - popover menu accordion', () => {
     it('should open and close menus', () => {
         cy.mount(html`<vl-popover-menu-accordion></vl-popover-menu-accordion>`);
 
-        cy.get('vl-popover-menu-accordion').find('a#btn-acties1').click();
+        cy.get('vl-popover-menu-accordion').find('vl-button-next#btn-acties1[ghost]').click();
         cy.get('vl-popover-menu-accordion').find('vl-popover[for="btn-acties1"]').should('be.visible');
-        cy.get('vl-popover-menu-accordion').find('a#btn-acties1').click();
+        cy.get('vl-popover-menu-accordion').find('vl-button-next#btn-acties1[ghost]').click();
         cy.get('vl-popover-menu-accordion').find('vl-popover[for="btn-acties1"]').should('not.be.visible');
-        cy.get('vl-popover-menu-accordion').find('a#btn-acties1').click();
+        cy.get('vl-popover-menu-accordion').find('vl-button-next#btn-acties1[ghost]').click();
         cy.get('vl-popover-menu-accordion').find('vl-popover[for="btn-acties1"]').should('be.visible');
         cy.get('body').click();
         cy.get('vl-popover-menu-accordion').find('vl-popover[for="btn-acties1"]').should('not.be.visible');
 
-        cy.get('vl-popover-menu-accordion').find('a#btn-acties2').click();
+        cy.get('vl-popover-menu-accordion').find('vl-button-next#btn-acties2').click();
         cy.get('vl-popover-menu-accordion').find('vl-popover[for="btn-acties2"]').should('be.visible');
-        cy.get('vl-popover-menu-accordion').find('a#btn-acties2').click();
+        cy.get('vl-popover-menu-accordion').find('vl-button-next#btn-acties2').click();
         cy.get('vl-popover-menu-accordion').find('vl-popover[for="btn-acties2"]').should('not.be.visible');
-        cy.get('vl-popover-menu-accordion').find('a#btn-acties2').click();
+        cy.get('vl-popover-menu-accordion').find('vl-button-next#btn-acties2').click();
         cy.get('vl-popover-menu-accordion').find('vl-popover[for="btn-acties2"]').should('be.visible');
         cy.get('body').click();
         cy.get('vl-popover-menu-accordion').find('vl-popover[for="btn-acties2"]').should('not.be.visible');
