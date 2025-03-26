7cbce27d9bb3e6db726baa05b8e3c02f4117b9e1
k-o-e-n <koen.buckinx@vlaanderen.be>
2025-03-26 17:00:54 +0100

    feat: UIG-3104 - vl-functional-header - voorbeelden toegevoegd met vl-button-next en vl-search

---
+++ apps/storybook-e2e/src/e2e/ontwerp/functional-header/demo-with-search/functional-header-demo-with-search.cy.ts
@@ -0,0 +1,10 @@
+const demoWithSearchUrl =
+    'http://localhost:8080/iframe.html?id=ontwerp-functional-header-voorbeeld-met-search--functional-header-met-search&viewMode=story';
+
+describe('story - functional header met search voorbeeld', () => {
+    it('should render', () => {
+        cy.visit(demoWithSearchUrl);
+
+        cy.get('vl-functional-header').shadow();
+    });
+});
