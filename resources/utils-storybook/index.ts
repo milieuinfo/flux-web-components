66ac19494aaee06c7050f444055b920bb78e6a7d
Kris Speltincx <kris.speltincx@vlaanderen.be>
2025-04-18 10:52:10 +0200

    feat: UIG-3269 - module verbeteringen
    - barrel-file toegevoegd in utils-storybook
    - path "@domg-wc/integration/*" weggewerkt, is nu "@domg-wc/integration/form" en "@domg-wc/integration/popover"
    - "@domg-wc/common/constants" weggewerkt uit tsconfig.base.json

---
+++ resources/utils-storybook/index.ts
@@ -0,0 +1,16 @@
+export {
+    CATEGORIES,
+    TYPES,
+    CONTROLS,
+    defaultArgs,
+    defaultArgTypes,
+    filterOutClasses,
+    filterOutDataCy,
+    formatHTML,
+    formattedSourceCode,
+    getSelectControlOptions,
+    logStorybookEvent,
+    story,
+} from './stories.helper';
+
+export { ICON_PLACEMENT, MARGINS, PADDINGS } from '../../libs/common/src/constants';
