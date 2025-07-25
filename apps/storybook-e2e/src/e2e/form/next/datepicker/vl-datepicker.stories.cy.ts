2b1bef0b7d83a0bd9548a6d5371becd3d5c2164e
k-o-e-n <koen.buckinx@vlaanderen.be>
2025-07-25 13:23:44 +0200

    fix: FLUX-135 - vl-datepicker-next - initial value werd genegeerd bij type "range"

--- apps/storybook-e2e/src/e2e/form/next/datepicker/vl-datepicker.stories.cy.ts
+++ apps/storybook-e2e/src/e2e/form/next/datepicker/vl-datepicker.stories.cy.ts
@@ -1,10 +1,11 @@
-const datepickerNextDefaultUrl =
-    'http://localhost:8080/iframe.html?args=&id=form-next-datepicker--datepicker-default&viewMode=story';
+['default', 'min-date-and-max-date', 'static', 'range', 'time', 'date-time'].forEach((story) => {
+    describe(`story - vl-datepicker-next - ${story}`, () => {
+        it('should render', () => {
+            cy.visit(
+                `http://localhost:8080/iframe.html?args=&id=form-next-datepicker--datepicker-${story}&viewMode=story`
+            );
 
-describe('story - vl-datepicker-next - default', () => {
-    it('should render', () => {
-        cy.visit(datepickerNextDefaultUrl);
-
-        cy.get('vl-datepicker-next').shadow();
+            cy.get('vl-datepicker-next').shadow();
+        });
     });
 });
