05b27293864f6fad99db0043daaaebc80a213b94
Karim Dehbi <karim.dehbi@gmail.com>
2025-07-25 14:05:04 +0200

    fix: FLUX-122 - vl-toaster - werking verbeterd
    De declaratieve manier van meldingen tonen is veranderd; in de vorige Lit variant van de `vl-toaster` werden meldingen pas getoond als de bijhorende functie werd aangeroepen. Nu worden die onmiddellijk getoond, deze werking is nu dezelfde hoe de vroegere `is="vl-toaster"` werkte.
    Daarnaast is ook ontbrekende witruimte toegevoegd en responsiveness op mobiel verbeterd.

--- apps/storybook-e2e/src/e2e/components/block/toaster/vl-toaster.stories.cy.ts
+++ apps/storybook-e2e/src/e2e/components/block/toaster/vl-toaster.stories.cy.ts
@@ -1,11 +1,13 @@
 const toasterDefaultUrl =
     'http://localhost:8080/iframe.html?args=&id=components-block-toaster--toaster-default&viewMode=story';
+const toasterDefaultSlottUrl =
+    'http://localhost:8080/iframe.html?args=&id=components-block-toaster--toaster-default-slot&viewMode=story';
 const toasterShowAlertUrl =
     'http://localhost:8080/iframe.html?args=&id=components-block-toaster--toaster-show-alert&viewMode=story';
 const toasterFadeOutStoryUrl =
     'http://localhost:8080/iframe.html?args=&id=components-block-toaster--toaster-fade-out&viewMode=story';
 
-describe('story vl-toaster', () => {
+describe('story - vl-toaster', () => {
     it('should render', () => {
         cy.visit(toasterDefaultUrl);
 
@@ -13,7 +15,15 @@ describe('story vl-toaster', () => {
     });
 });
 
-describe('story vl-toaster show alert', () => {
+describe('story - vl-toaster - default slot', () => {
+    it('should render', () => {
+        cy.visit(toasterDefaultSlottUrl);
+
+        cy.get('vl-toaster').shadow().find('output');
+    });
+});
+
+describe('story - vl-toaster - show alert', () => {
     it('should render', () => {
         cy.visit(toasterShowAlertUrl);
 
@@ -21,7 +31,7 @@ describe('story vl-toaster show alert', () => {
     });
 });
 
-describe('story vl-toaster fade out', () => {
+describe('story - vl-toaster - fade out', () => {
     it('should render', () => {
         cy.visit(toasterFadeOutStoryUrl);
 
--- libs/components/src/block/toaster/vl-toaster.component.ts
+++ libs/components/src/block/toaster/vl-toaster.component.ts
@@ -10,10 +10,12 @@ registerWebComponents([VlAlert]);
 
 @webComponent('vl-toaster')
 export class VlToasterComponent extends BaseLitElement {
+    // properties
     private fadeOut = toasterDefaults.fadeOut;
     private placement: Placement = toasterDefaults.placement;
-
+    // state
     private abortController = new AbortController();
+    private initialized = false;
 
     static get styles(): (CSSResult | CSSResult[])[] {
         return [toasterStyles];
@@ -27,23 +29,36 @@ export class VlToasterComponent extends BaseLitElement {
     }
 
     render(): HTMLTemplateResult {
-        return html` <output class="vl-toaster"></output> `;
+        return html`
+            <output class="vl-toaster">
+                <slot></slot>
+            </output>
+        `;
     }
 
     disconnectedCallback() {
         this.abortController.abort('animationend');
     }
 
+    /**
+     * Wanneer aangeroepen zonder argument, wordt het eerste element in de toaster getoond.
+     * Je kan ook een specifiek element tonen door een selector mee te geven of een HTMLElement.
+     * @param target
+     */
     show(target?: string | HTMLElement): void {
         let toast;
         const firstChild = this.children[0]?.cloneNode(true) as HTMLElement;
         if (!target && firstChild) {
             toast = firstChild;
+            // we verwijderen het id attribuut op de gekloonde node om dubbele id's te voorkomen
+            toast?.removeAttribute('id');
+            toast?.removeAttribute('slot');
         } else if (target && typeof target === 'string') {
-            const foundElement = this.querySelector(target);
+            const foundElement = document.querySelector(target);
             toast = foundElement?.cloneNode(true) as HTMLElement;
             // we verwijderen het id attribuut op de gekloonde node om dubbele id's te voorkomen
-            toast.removeAttribute('id');
+            toast?.removeAttribute('id');
+            toast?.removeAttribute('slot');
         } else if (target instanceof HTMLElement) {
             toast = target as HTMLElement;
         }
@@ -54,6 +69,10 @@ export class VlToasterComponent extends BaseLitElement {
         this.showToast(toast);
     }
 
+    /**
+     * Toont een vl-alert in de toaster met de opgegeven attributen/properties.
+     * @param alert
+     */
     showAlert(alert: VlAlertModel) {
         const vlAlert = document.createElement('vl-alert');
         Object.entries(alert).forEach(([key, value]) => {
@@ -61,17 +80,25 @@ export class VlToasterComponent extends BaseLitElement {
                 vlAlert.setAttribute(`${key}`, value);
             }
         });
-        this.appendChild(vlAlert);
         this.showToast(vlAlert);
     }
 
-    private showToast(toast: HTMLElement) {
-        if (this.placement === 'bottom-right' || this.placement === 'bottom-left') {
-            this.shadowRoot?.appendChild(toast);
-        } else {
-            this.shadowRoot?.prepend(toast);
+    private get outputElement(): HTMLElement | null | undefined {
+        return this.shadowRoot?.querySelector('output');
+    }
+
+    private showToast = async (toast: HTMLElement) => {
+        // wacht tot <output> element is gerenderd
+        if (!this.initialized) {
+            this.initialized = true;
+            await this.updateComplete;
         }
 
+        if (this.placement === 'bottom-right' || this.placement === 'bottom-left') {
+            this.outputElement?.appendChild(toast);
+        } else {
+            this.outputElement?.prepend(toast);
+        }
         if (this.fadeOut) {
             toast.addEventListener(
                 'animationend',
@@ -83,7 +110,7 @@ export class VlToasterComponent extends BaseLitElement {
                 { signal: this.abortController.signal }
             );
         }
-    }
+    };
 }
 
 declare global {
