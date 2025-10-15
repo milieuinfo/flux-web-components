fddfde966d1c5653608a2a8ef1ad217cc7b484ab
Karim Dehbi <karim.dehbi@gmail.com>
2025-10-15 14:14:58 +0200

    fix: FLUX-121 - vl-select-next - declaratieve options toegevoegd

--- libs/form/src/next/select/stories/vl-select.stories.ts
+++ libs/form/src/next/select/stories/vl-select.stories.ts
@@ -164,3 +164,59 @@ SelectReadOnly.args = {
         { label: 'Rio Piedras', value: 'rio piedras', disabled: true },
     ],
 };
+
+const SelectDeclarativeOptionsTemplate = story(
+    selectArgs,
+    ({
+        id,
+        name,
+        label,
+        required,
+        disabled,
+        error,
+        success,
+        placeholder,
+        notDeletable,
+        autocomplete,
+        block,
+        onVlChange,
+        onVlInput,
+        onVlValid,
+        onVlReset,
+    }) => {
+        return html` <vl-select-next
+            id=${id}
+            name=${name}
+            label=${label}
+            ?required=${required}
+            ?disabled=${disabled}
+            ?error=${error}
+            ?success=${success}
+            placeholder=${placeholder}
+            ?not-deletable=${notDeletable}
+            ?block=${block}
+            autocomplete=${autocomplete}
+            @vl-change=${onVlChange}
+            @vl-input=${onVlInput}
+            @vl-valid=${onVlValid}
+            @vl-reset=${onVlReset}
+        >
+            <option value="antwerpen">Antwerpen</option>
+            <option value="brussel" selected>Brussel</option>
+            <option value="gent">gent</option>
+            <option value="hasselt" disabled>Hasselt (niet beschikbaar)</option>
+            <option value="waregem">Waregem</option>
+            <option value="lier">Lier</option>
+            <option value="rio-piedras">Rio Piedras</option>
+        </vl-select-next>`;
+    }
+);
+
+export const SelectDeclarativeOptions = SelectDeclarativeOptionsTemplate.bind({});
+SelectDeclarativeOptions.storyName = 'vl-select-next - declarative options';
+SelectDeclarativeOptions.args = {
+    id: 'woonplaats',
+    name: 'woonplaats',
+    label: 'Kies je woonplaats',
+    placeholder: 'Kies je woonplaats',
+};
