b5b78c1e2e5bbfe024c85f61b3a2b04e25e3f560
k-o-e-n <koen.buckinx@vlaanderen.be>
2025-11-17 13:32:15 +0100

    fix: FLUX-358 - vl-properties - columns layout brak uit zijn parent container

--- libs/components/src/block/properties/vl-properties.css.ts
+++ libs/components/src/block/properties/vl-properties.css.ts
@@ -77,7 +77,8 @@ export const propertiesStyles: CSSResult = css`
     }
 
     dl:has(.item) {
-        display: initial;
+        display: flow-root;
+        margin-block: 0;
     }
 
     dl .item {
