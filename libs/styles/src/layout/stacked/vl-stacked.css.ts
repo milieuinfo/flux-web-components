185f5df69070909be2900fdc4aa35327fcb17086
Kris Speltincx <kris.speltincx@vlaanderen.be>
2026-03-02 15:36:55 +0100

    feat: FLUX-482 - vl-stacked - introductie van losstaande 'vl-stacked'

--- libs/styles/src/layout/stacked/vl-stacked.css.ts
+++ libs/styles/src/layout/stacked/vl-stacked.css.ts
@@ -2,6 +2,15 @@ import { css, CSSResult } from 'lit';
 import { vlMediaScreenMedium } from '../../base/var/vl-media-screen.css';
 
 export const vlStackedStyles: CSSResult = css`
+    .vl-stacked {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .vl-stacked-small {
+        row-gap: var(--vl-spacing--small);
+    }
+
     .vl-stacked-medium {
         row-gap: var(--vl-spacing--medium);
     }
@@ -12,9 +21,5 @@ export const vlStackedStyles: CSSResult = css`
         @media screen and (max-width: ${vlMediaScreenMedium}px) {
             row-gap: var(--vl-spacing--medium);
         }
-    }
-
-    .vl-stacked-small {
-        row-gap: var(--vl-spacing--small);
     }
 `;
