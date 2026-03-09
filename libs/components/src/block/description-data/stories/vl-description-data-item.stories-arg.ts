991d46a712c62f20991c0e52b3de779aa228ac47
Kris Speltincx <kris.speltincx@vlaanderen.be>
2026-03-09 14:18:02 +0100

    feat: FLUX-335 - vl-description-data - size's op het item krijgen voorrang

--- libs/components/src/block/description-data/stories/vl-description-data-item.stories-arg.ts
+++ libs/components/src/block/description-data/stories/vl-description-data-item.stories-arg.ts
@@ -1,4 +1,4 @@
-import { defaultArgs, defaultArgTypes, TYPES } from '@resources/utils-storybook';
+import { CATEGORIES, defaultArgs, defaultArgTypes, TYPES } from '@resources/utils-storybook';
 import { ArgTypes } from '@storybook/web-components-vite';
 
 export const descriptionDataItemArgs = {
@@ -7,10 +7,58 @@ export const descriptionDataItemArgs = {
     value: 'Kind en Gezin',
     labelSlotText: 'Uitgever',
     valueSlotText: 'Kind en Gezin',
+    itemsSize: undefined,
+    itemsMediumSize: undefined,
+    itemsSmallSize: undefined,
+    itemsExtraSmallSize: undefined,
 };
 
 export const descriptionDataItemArgTypes: ArgTypes<typeof descriptionDataItemArgs> = {
     ...defaultArgTypes,
+    itemsSize: {
+        name: 'items-size',
+        type: { name: TYPES.NUMBER },
+        description:
+            'Kolombreedte van dit item op grote schermen. Overschrijft het `items-size` attribuut van de parent `vl-description-data`.',
+        table: {
+            type: { summary: TYPES.NUMBER },
+            category: CATEGORIES.ATTRIBUTES,
+            defaultValue: { summary: undefined },
+        },
+    },
+    itemsMediumSize: {
+        name: 'items-medium-size',
+        type: { name: TYPES.NUMBER },
+        description:
+            'Kolombreedte van dit item op middelgrote schermen. Overschrijft het `items-medium-size` attribuut van de parent `vl-description-data`.',
+        table: {
+            type: { summary: TYPES.NUMBER },
+            category: CATEGORIES.ATTRIBUTES,
+            defaultValue: { summary: undefined },
+        },
+    },
+    itemsSmallSize: {
+        name: 'items-small-size',
+        type: { name: TYPES.NUMBER },
+        description:
+            'Kolombreedte van dit item op kleine schermen. Overschrijft het `items-small-size` attribuut van de parent `vl-description-data`.',
+        table: {
+            type: { summary: TYPES.NUMBER },
+            category: CATEGORIES.ATTRIBUTES,
+            defaultValue: { summary: undefined },
+        },
+    },
+    itemsExtraSmallSize: {
+        name: 'items-extra-small-size',
+        type: { name: TYPES.NUMBER },
+        description:
+            'Kolombreedte van dit item op zeer kleine schermen. Overschrijft het `items-extra-small-size` attribuut van de parent `vl-description-data`.',
+        table: {
+            type: { summary: TYPES.NUMBER },
+            category: CATEGORIES.ATTRIBUTES,
+            defaultValue: { summary: undefined },
+        },
+    },
     label: {
         name: 'label',
         description: 'Changes the label of the data item.',
