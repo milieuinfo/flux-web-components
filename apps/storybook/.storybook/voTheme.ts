6efb57bb9b11e0f2b66b3753d78c9bea4dd0c992
k-o-e-n <koen.buckinx@vlaanderen.be>
2026-03-13 11:16:32 +0100

    lab: storybook theming

--- apps/storybook/.storybook/voTheme.ts
+++ apps/storybook/.storybook/voTheme.ts
@@ -2,7 +2,18 @@ import { create } from 'storybook/theming';
 
 export default create({
     base: 'light',
-    brandTitle: 'Vlaamse Overheid',
-    brandUrl: 'https://www.vlaanderen.be/',
-    brandImage: 'logo-vo.png',
+    brandTitle: 'Departement Omgeving - FLUX - Web Componenten',
+    brandUrl: 'https://omgeving.vlaanderen.be/',
+    brandImage: 'storybook-flux-logo.svg',
+    appBg: '#F6F6F3', // achtergrond van de UI (incl. navigatie)
+    barBg: '#F6F6F3', // bovenste toolbar achtergrond
+    textColor: '#333332', // algemene tekstkleur
+    colorPrimary: '#447A6D', // accent kleur (knoppen, highlights)
+    colorSecondary: '#447A6D',
+    appContentBg: '#FFFFFF',
+    appBorderColor: '#447A6D22',
+    inputBg: '#FFFFFF',
+    inputBorder: '#33333233',
+    inputTextColor: '#333332',
+    barSelectedColor: '#447A6D',
 });
