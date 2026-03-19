be2b78d2c3ffa5b1c2435e66b1cb5e2b9d3a2f4e
Karim Dehbi <karim.dehbi@gmail.com>
2026-03-19 11:23:06 +0100

    fix: FLUX-551 - storybook - arg-files  verbeterd
    Ontbrekende `category: CATEGORIES.ATTRIBUTES` toegevoegd aan stories-arg bestanden waardoor attributen niet in de web-types JSON werden opgenomen.
    
    Aangepaste componenten:
    - vl-side-sheet (10 attributen)
    - vl-infoblock (4 attributen)
    - vl-share-button (2 attributen)
    - vl-template (2 attributen)
    - vl-typography (1 attribuut)

--- libs/components/src/block/share-buttons/stories/vl-share-button.stories-arg.ts
+++ libs/components/src/block/share-buttons/stories/vl-share-button.stories-arg.ts
@@ -1,4 +1,4 @@
-import { CONTROLS, defaultArgs, defaultArgTypes, getSelectControlOptions } from '@resources/utils-storybook';
+import { CATEGORIES, CONTROLS, defaultArgs, defaultArgTypes, getSelectControlOptions, TYPES } from '@resources/utils-storybook';
 import { ArgTypes } from '@storybook/web-components-vite';
 import { MEDIA } from '../vl-share-button.model';
 
@@ -15,7 +15,17 @@ export const shareButtonArgTypes: ArgTypes = {
         control: { type: CONTROLS.SELECT },
         options: Object.values(MEDIA),
         description: 'This attribute is used to pass the medium.',
-        table: { type: { summary: getSelectControlOptions(Object.values(MEDIA)) } },
+        table: {
+            type: { summary: getSelectControlOptions(Object.values(MEDIA)) },
+            category: CATEGORIES.ATTRIBUTES,
+        },
     },
-    href: { type: 'string', description: "The href attribute specifies the link's destination." },
+    href: {
+        name: 'href',
+        description: "The href attribute specifies the link's destination.",
+        table: {
+            type: { summary: TYPES.STRING },
+            category: CATEGORIES.ATTRIBUTES,
+        },
+    },
 };
