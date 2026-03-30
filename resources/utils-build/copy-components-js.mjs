30973877764ec977640cffb80910188eb7a24dc7
Karim Dehbi <karim.dehbi@gmail.com>
2026-03-30 18:00:55 +0200

    fix: FLUX-604 - components build - raw css bestanden toevoegen aan dist

--- resources/utils-build/copy-components-js.mjs
+++ resources/utils-build/copy-components-js.mjs
@@ -4,5 +4,6 @@ import { copyFiles } from './file-processors.mjs';
 
 copyFiles('build/tsc/lib-components/components/src', 'build/dist/libs/components', '');
 copyFiles('libs/components/src', 'build/dist/libs/components', '.js');
+copyFiles('libs/components/src', 'build/dist/libs/components', '.css');
 copyFiles('libs/components', 'build/dist/libs/components', 'package.json');
 copyFiles('libs/components/src', 'build/dist/libs/components', '.web-types.json');
--- resources/utils-build/copy-map-js.mjs
+++ resources/utils-build/copy-map-js.mjs
@@ -4,5 +4,6 @@ import { copyFiles } from './file-processors.mjs';
 
 copyFiles('build/tsc/lib-map/map/src', 'build/dist/libs/map', '');
 copyFiles('libs/map/src', 'build/dist/libs/map', '.js');
+copyFiles('libs/map/src', 'build/dist/libs/map', '.css');
 copyFiles('libs/map', 'build/dist/libs/map', 'package.json');
 copyFiles('libs/map', 'build/dist/libs/map', '.web-types.json');
