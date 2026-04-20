d6a0c811f4ac4d83226c7bb7bf9438762f5ce5c6
k-o-e-n <koen.buckinx@vlaanderen.be>
2026-04-20 17:57:37 +0200

    feat: FLUX-278 - vl-title - appearance attribuut

--- apps/storybook-e2e/src/e2e/components/atom/title/vl-title.stories.cy.ts
+++ apps/storybook-e2e/src/e2e/components/atom/title/vl-title.stories.cy.ts
@@ -1,8 +1,46 @@
 const titleDefaultUrl = 'http://localhost:8080/iframe.html?id=components-atom-title--title-default&viewMode=story';
+const titleAppearanceUrl =
+    'http://localhost:8080/iframe.html?id=components-atom-title--title-appearance&viewMode=story';
+const titleUnderlineUrl = 'http://localhost:8080/iframe.html?id=components-atom-title--title-underline&viewMode=story';
+const titleNoSpaceBottomUrl =
+    'http://localhost:8080/iframe.html?id=components-atom-title--title-no-space-bottom&viewMode=story';
+const titleAltUrl = 'http://localhost:8080/iframe.html?id=components-atom-title--title-alt&viewMode=story';
 
 describe('cypress-e2e - atom components - vl-title - default story', () => {
     it('should render', () => {
         cy.visit(titleDefaultUrl);
+
+        cy.get('vl-title').shadow();
+    });
+});
+
+describe('cypress-e2e - atom components - vl-title - appearance story', () => {
+    it('should render', () => {
+        cy.visit(titleAppearanceUrl);
+
+        cy.get('vl-title').shadow();
+    });
+});
+
+describe('cypress-e2e - atom components - vl-title - underline story', () => {
+    it('should render', () => {
+        cy.visit(titleUnderlineUrl);
+
+        cy.get('vl-title').shadow();
+    });
+});
+
+describe('cypress-e2e - atom components - vl-title - no-space-bottom story', () => {
+    it('should render', () => {
+        cy.visit(titleNoSpaceBottomUrl);
+
+        cy.get('vl-title').shadow();
+    });
+});
+
+describe('cypress-e2e - atom components - vl-title - alt story', () => {
+    it('should render', () => {
+        cy.visit(titleAltUrl);
 
         cy.get('vl-title').shadow();
     });
