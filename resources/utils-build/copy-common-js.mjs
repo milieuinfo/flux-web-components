8eda99ffcbc977f8dbec391b22b63f6a39878a55
Kris Speltincx <kris.speltincx@vlaanderen.be>
2026-05-28 12:53:04 +0200

    fix: FLUX-703 - web-types - hernoeming van de template package.json's

--- resources/utils-build/copy-common-js.mjs
+++ resources/utils-build/copy-common-js.mjs
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-import { copyFiles } from './file-processors.mjs';
+import { copyFile, copyFiles } from './file-processors.mjs';
 
 copyFiles('build/tsc/lib-common/common/src', 'build/dist/libs/common', '');
 copyFiles('libs/common/src', 'build/dist/libs/common', '.js');
 copyFiles('libs/common/src', 'build/dist/libs/common', '.css');
-copyFiles('libs/common', 'build/dist/libs/common', 'package.json');
 copyFiles('libs/common', 'build/dist/libs/common', '.web-types.json');
+copyFile('libs/common/package.template.json', 'build/dist/libs/common/package.json');
