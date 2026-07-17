bd0800554e451d65faf7e6a683266ec1b82ce242
Karim Dehbi <karim.dehbi@gmail.com>
2026-07-17 11:17:08 +0200

    feat: libs als pnpm workspace met workspace:* interne deps
    Elke lib (common, styles, components, map) krijgt een echte package.json
    als workspace-member met workspace:* voor de interne @domg-wc deps, in
    plaats van de package.template.json met DOMG-WC-VERSION placeholder.
    libs/integrations krijgt een private package.json zodat de libs/* glob
    proper werkt (nooit gepubliceerd).
    
    libs-pack.sh gebruikt pnpm's eigen publish-rewrite via publishConfig.directory:
    pnpm packt de gebouwde dist-output vanuit de workspace-member en herschrijft
    workspace:* naar de concrete release-versie. sed op package.json en de
    pnpm pkg set sideEffects vervallen (sideEffects staat nu statisch in elke
    manifest). libs-publish.sh blijft ongewijzigd.

--- resources/utils-build/copy-common-js.mjs
+++ resources/utils-build/copy-common-js.mjs
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
-import { copyFile, copyFiles } from './file-processors.mjs';
+import { copyFiles } from './file-processors.mjs';
 
 copyFiles('build/tsc/lib-common/common/src', 'build/dist/libs/common', '');
 copyFiles('libs/common/src', 'build/dist/libs/common', '.js');
 copyFiles('libs/common/src', 'build/dist/libs/common', '.css');
 copyFiles('libs/common', 'build/dist/libs/common', '.web-types.json');
-copyFile('libs/common/package.template.json', 'build/dist/libs/common/package.json');
