7eb9ae6429381a3eeb584a0f7aa2a41477367399
Kris Speltincx <kris.speltincx@vlaanderen.be>
2026-07-24 14:34:34 +0200

    build: FLUX-643 - Jenkins build

--- resources/ci-bamboo/bash/e2e-tests-storybook.sh
+++ resources/ci-bamboo/bash/e2e-tests-storybook.sh
@@ -4,7 +4,7 @@
 set -e
 
 echo 'RUNNING SCRIPT: e2e-tests-storybook.sh'
-cd flux-web-components
+cd "$(dirname "$0")/../../.."
 
 echo "npm ci - to force the clean"
 set +e
@@ -22,7 +22,9 @@ fi
 set -e
 
 echo "create build folder with dummy text file - when everything goes well there is no build folder which fails the build"
-mkdir build
+# -p: deze stage draait in een eigen workspace, maar -p houdt het script ook
+# bruikbaar bij een lokale herhaalde run waar build/ al bestaat
+mkdir -p build
 touch build/dummy.txt
 
 echo "serve storybook and run the e2e tests"
